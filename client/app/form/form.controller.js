e446a48af55032ef6dbe19ca4dfc23be5d76f8d1
Aniruddh Agarwal <aaniruddh99@gmail.com>
2015-11-18 23:56:23 +0800

    Add save data feature for forms

--- client/app/form/form.controller.js
+++ client/app/form/form.controller.js
@@ -2,73 +2,129 @@
 
 angular.module('thinkKidsCertificationProgramApp')
   .controller('FormCtrl', function ($scope, $http, $stateParams, $location, Auth) {
-    $scope.viewForm = false;
-
-    $scope.saveForm = function(data) {
-        $http.post('/api/forms', { name: data.formName, data: [data], roles: []}).success(function(form) {
-            $location.path("/form/" + form._id + "/roles");
-        });
-    };
-
-    if($stateParams.formID) {
+    if($stateParams.id) {
+      if(window.location.pathname.indexOf("roles") > -1) {
         $http.get('/api/roles')
-            .success(function(roles) {
-                $http.get('/api/forms/' + $stateParams.formID)
-                    .success(function(form) {
-                        $scope.roles = roles.map(function(role) {
-                            if(form.roles.indexOf(role.name) !== -1) {
-                                role.permitted = true;
-                            } else {
-                                role.permitted = false;   
-                            }
-                            return role;
-                        });
-                        $scope.roles = $scope.roles.splice(3, $scope.roles.length-3);
-                    })
-            });
+          .success(function(roles) {
+            $http.get('/api/forms/' + $stateParams.id)
+              .success(function(form) {
+                $scope.roles = roles.map(function(role) {
+                  if(form.roles.indexOf(role.name) !== -1) {
+                    role.permitted = true;
+                  } else {
+                    role.permitted = false;   
+                  }
+                  return role;
+                });
+                $scope.roles = $scope.roles.splice(3, $scope.roles.length-3);
+              })
+          });
         
         $scope.saveRoles = function(data) {
-            var roles = data.filter(function(role) {
-                return role.permitted === true;
-            }).map(function(role) {
-                return role.name;
+          var roles = data.filter(function(role) {
+            return role.permitted === true;
+          }).map(function(role) {
+            return role.name;
+          });
+          
+          $http.patch('/api/forms/'+$stateParams.id, {roles: roles})
+            .success(function(form) {
+              $location.path("/admin");
             });
+         };
+      } else if (window.location.pathname.indexOf("data") > -1) {
+        $http.get("/api/forms/" + $stateParams.id)
+          .success(function(form) {
+            $scope.form = form;
             
-            $http.patch('/api/forms/'+$stateParams.formID, {roles: roles})
-                .success(function(form) {
-                   location.path("/admin");
-                });
-         };  
-    }
-
-    if($stateParams.id) {
+            if($stateParams.by) {
+              $scope.showData = false;
+              
+             $scope.form.submittedData = $scope.form.submittedData.filter(function(data) {
+                return data.by === $stateParams.by;
+              });
+              $scope.form.submittedData = $scope.form.submittedData[0];
+              
+              var dataProps = Object.getOwnPropertyNames($scope.form.submittedData);
+              var data = [];
+              
+              for(var i = 0; i < dataProps.length; i++) {
+                if(dataProps[i] === "byName") {
+                  continue;
+                } else if(dataProps[i] === "by") {
+                  continue;
+                } else {
+                  var tempData = {};
+                  tempData.prop = dataProps[i];
+                  tempData.val = $scope.form.submittedData[dataProps[i]];
+                  data.push(tempData);
+                }
+              }
+              $scope.data = data;
+              console.log(data);
+            } else {
+              $scope.showData = true;
+              $scope.deleteData = function(data) {
+                form.submittedData.splice(form.submittedData.indexOf(data), 1);
+                var submittedData = form.submittedData;
+                $http.patch('/api/forms/'+$stateParams.id, {submittedData: submittedData});
+              }
+            }
+          });
+      } else if (window.location.pathname.indexOf("data") === -1 && window.location.pathname.indexOf("roles") === -1) {
         $scope.viewForm = true;
         
         $http.get('/api/forms/'+$stateParams.id)
-            .success(function(form) {
-        
-                var permitted = form.roles.filter(function(role) {
-                    return Auth.getCurrentUser().roles.indexOf(role) !== -1;
-                }).length;
-        
-                if(permitted == 0) {
-                    $location.path("/");
-                } else {
-                    $scope.form = {};
-                    $scope.form.btnSubmitText = form.data[0].btnSubmitText;
-                    $scope.form.btnCancelText = form.data[0].btnCancelText;
-                    $scope.form.fieldsModel = form.data[0].edaFieldsModel;
-                    $scope.form.dataModel = {};
-        
-                    $scope.form.submitFormEvent = function(dataSubmitted) {
-                        console.log("Form submitted");
-                        console.log("Here's your data: " + dataSubmitted);
-                    };
-        
-                    $scope.form.cancelFormEvent = function() {
-                        console.log("Form cancelled!");
-                    };
+          .success(function(form) {      
+            var permitted = form.roles.filter(function(role) {
+              return Auth.getCurrentUser().roles.indexOf(role) !== -1;
+            }).length;
+    
+            if(permitted == 0) {
+              $location.path("/");
+            } else {
+              $scope.form = {};
+              $scope.form.btnSubmitText = form.data[0].btnSubmitText;
+              $scope.form.btnCancelText = form.data[0].btnCancelText;
+              $scope.form.fieldsModel = form.data[0].edaFieldsModel;
+              $scope.form.dataModel = {};
+
+              $scope.form.submitFormEvent = function(dataSubmitted) {
+                var formFieldsData = form.data[0].edaFieldsModel;
+                var formSubmittedDataProps = Object.getOwnPropertyNames($scope.form.dataModel);
+                var formSubmittedData = {};
+                    
+                for(var i = 0; i < formSubmittedDataProps.length; i++) {
+                  for(var x = 1; x < formFieldsData.length; x++) {
+                    for(var y = 0; y < formFieldsData[x].columns.length; y++) {
+                      if(formFieldsData[x].columns[y].control.key === formSubmittedDataProps[i]) {
+                        formSubmittedData[formFieldsData[x].columns[y].control.templateOptions.label] = $scope.form.dataModel[formSubmittedDataProps[i]];        
+                      }
+                    }
+                  }
                 }
-            });
+                    
+                formSubmittedData["by"] = Auth.getCurrentUser()._id;
+                formSubmittedData["byName"] = Auth.getCurrentUser().name;
+                
+                form.submittedData.push(formSubmittedData);
+                formSubmittedData = form.submittedData;
+                $http.patch('/api/forms/'+$stateParams.id, {submittedData: formSubmittedData});
+              };
+    
+              $scope.form.cancelFormEvent = function() {
+                console.log("Form cancelled!");
+              };
+            }
+          });
+      }
+    } else {
+      $scope.viewForm = false;
+
+      $scope.saveForm = function(data) {
+        $http.post('/api/forms', { name: data.formName, submittedData: [], data: [data], roles: []}).success(function(form) {
+          $location.path("/form/" + form._id + "/roles");
+        });
+      };
     }
   });
--- server/api/form/index.js
+++ server/api/form/index.js
@@ -10,7 +10,7 @@ router.get('/', controller.index);
 router.get('/:id', controller.show);
 router.post('/', auth.can("create_forms"), controller.create);
 router.put('/:id', auth.can("create_forms"), controller.update);
-router.patch('/:id', auth.can("create_forms"), controller.update);
+router.patch('/:id', controller.update);
 router.delete('/:id', auth.can("delete_forms"), controller.destroy);
 
 module.exports = router;
