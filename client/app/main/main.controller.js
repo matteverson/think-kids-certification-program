a80f48d41868887edf6aa31238a3d41f89fd99d3
Matt Everson <meverson@tenable.com>
2015-12-16 15:52:42 -0600

    Display form when selected on the main page

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,10 +1,18 @@
 'use strict';
 
 angular.module('thinkKidsCertificationProgramApp')
-  .controller('MainCtrl', function ($scope, $http) {
-    $scope.forms = [];
+.controller('MainCtrl', function ($scope, $http) {
+  $scope.forms = [];
 
-    $http.get('/api/forms/mine').success(function(forms) {
-      $scope.forms = forms;
-    });
+  $http.get('/api/forms/mine').success(function(forms) {
+    $scope.forms = forms;
   });
+
+  $scope.viewForm = function(form) {
+    $scope.form = {};
+    $scope.form.btnSubmitText = form.data[0].btnSubmitText;
+    $scope.form.btnCancelText = form.data[0].btnCancelText;
+    $scope.form.fieldsModel = form.data[0].edaFieldsModel;
+    $scope.form.dataModel = {};
+  }
+});
