dd040bce79bc05366ff79298964366512dc78bfc
Aniruddh Agarwal <aaniruddh99@gmail.com>
2016-02-22 14:22:50 +0800

    Enhancements

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -11,7 +11,6 @@ angular.module('thinkKidsCertificationProgramApp')
     });
 
     if(Auth.isLoggedIn()) {
-<<<<<<< 91ed5c025e7721d27f5dba9309b8650404da4bfb
       $http.get('/api/users/'+Auth.getCurrentUser()._id)
         .success(function(user) {
           $scope.newAnnouncement = function() {
@@ -24,24 +23,6 @@ angular.module('thinkKidsCertificationProgramApp')
             if(announcements.length > 0) {
               return true;
             }
-=======
-      $timeout(function() {
-        $scope.newAnnouncement = false;
-        $scope.newMessage = false;
-      });
-
-      $http.get('/api/users/'+Auth.getCurrentUser()._id)
-        .success(function(user) {
-          var announcements = user.announcements.filter(function(item) {
-            return !item.read;
-          });
-
-          if(announcements.length > 0) {
-            $timeout(function() {
-              $scope.newAnnouncement = true;
-            });
-          }
->>>>>>> Add heading to top bar
 
             return false;
           };
@@ -55,16 +36,8 @@ angular.module('thinkKidsCertificationProgramApp')
               return true;
             }
 
-<<<<<<< 91ed5c025e7721d27f5dba9309b8650404da4bfb
             return false;
           };
-=======
-          if(messages.length > 0) {
-            $timeout(function() {
-              $scope.newMessage = true;
-            });
-          }
->>>>>>> Add heading to top bar
       });
     }
 
